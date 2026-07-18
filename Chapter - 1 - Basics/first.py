f6db38cf5b1133791f041265265920335cd69724
Dave-Raaj <daveraajkolhi@gmail.com>
2026-07-18 23:09:22 +0500

    my calculator

--- Chapter - 1 - Basics/first.py
+++ Chapter - 1 - Basics/first.py
@@ -1,1 +1,10 @@
-print('Hello')
+num1 = int(input('Enter your number: '))
+num2 = int(input('Enter your number: '))
+
+
+
+def sumTwoNum(x,y):
+    return x - y
+
+r = sumTwoNum(num1, num2)
+print(r)
